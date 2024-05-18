6bc618abe77bd97c2534913581923bb2561c9d91
Lenin <Lenin>
2024-05-18 19:18:55 +0530

    binds annotation

--- app/src/main/java/com/example/myfirstapplication/di/cheezyCode/UserRepositoryModule.kt
+++ app/src/main/java/com/example/myfirstapplication/di/cheezyCode/UserRepositoryModule.kt
@@ -1,12 +1,22 @@
 package com.example.myfirstapplication.di.cheezyCode
 
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 
-@Module
+/*@Module
 class UserRepositoryModule {
-    @Provides
-    fun getFirebaseRepository(): UserRepository {
-        return FirebaseRepository()
+    @Provides // as we are using inject in sqlRepo constructor, so just pass sqlRepository: SQLRepository
+    fun getSQLRepository(sqlRepository: SQLRepository): UserRepository {
+        return sqlRepository
     }
+}*/
+
+// Another way by using binds, it need constructor injection, which sqlRepo have
+
+@Module
+abstract class UserRepositoryModule {
+    @Binds // all binds uses abstract and passes param need to have constructor injection like sqlRe..
+    abstract fun getSQLRepository(sqlRepository: SQLRepository): UserRepository
 }
+
