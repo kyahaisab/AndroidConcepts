4ad79519eb562ff8ff15dcea6a2f08a54fa26d32
Lenin <Lenin>
2024-06-30 11:13:24 +0530

    modifies data class

--- app/src/main/java/com/example/myfirstapplication/kotlinOOP/DataClass.kt
+++ app/src/main/java/com/example/myfirstapplication/kotlinOOP/DataClass.kt
@@ -2,14 +2,26 @@ package com.example.myfirstapplication.kotlinOOP
 
 // In java we have POJO classes, that are designed to store data only with getter and setters
 // In kotlin its simplified
+
+open class Detail {
+    open fun giveInfo() {
+        println("Hello")
+    }
+}
+
+// Can Inherit classes as well, but not data class
 data class PersonDetail(
     var name: String,
     var age: Int,
     var race: String
-) {
+) : Detail() {
     // You can have body of data class as well if required
     fun getFullDetail(): String {
         return "$name $age $race"
+    }
+
+    override fun giveInfo() {
+
     }
 }
 
